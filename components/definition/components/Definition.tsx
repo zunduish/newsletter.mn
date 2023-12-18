3c4324bc788fee7e6e2d852e80dcc5f67701f2e5
zundui <zundui@zunduis-MacBook-Pro.local>
2023-12-18 13:12:15 +0800

    font fix

--- components/definition/components/Definition.tsx
+++ components/definition/components/Definition.tsx
@@ -6,11 +6,17 @@ export default function Definition() {
     <div className="w-full m-auto px-[24px]">
       <div className="w-full border-b border-neutral-100 md:py-[70px]  sm:py-[50px] py-[50px]">
         <p className="display1">Цахим товхимлууд</p>
-        <p className="flex">
+        <div className="flex">
           <span className="metadata">Starting point of </span>
-          <span className="tag"> &nbsp;more &nbsp; </span>
-          <Image width={12} height={12} src="./images/orange_dot.svg" alt="" />
-        </p>
+          <span className="tag"> &nbsp;more </span>
+          <Image
+            width={12}
+            height={12}
+            src="./images/sys_images/orange_dot.svg"
+            alt=""
+            className="mt-[0px] ml-[7px] mb-[8px]"
+          />
+        </div>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import Header from "../components/layout/header/Header";
-import MainBody from "../components/layout/mainbody/MainBody";
+import HomeBody from "../components/layout/homebody/HomeBody";
 import Footer from "../components/layout/footer/Footer";
 
 export default function Home() {
   return (
     <main className="w-full">
       <Header />
-      <MainBody />
+      <HomeBody />
       <Footer />
     </main>
   );
